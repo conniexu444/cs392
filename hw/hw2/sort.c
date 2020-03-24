ff300270e710bedf643cda5c7df37aa6c2385ba5
conniexu444 <cxu16@stevens.edu>
2020-03-24 10:12:59 -0400

    started hw2

---
+++ hw/hw2/sort.c
@@ -0,0 +1,9 @@
+/**
+ * Constance Xu
+ * I pledge my honor that I have abided by the Stevens Honor System. 
+ * Task 2: sort numbers from input file and put them in an output file
+*/
+#include <stdlib.h>
+
+int main(){
+}
