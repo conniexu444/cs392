5dddf36825115debba26ef215eb46b7815a6a415
Constance Xu <43225884+conniexu444@users.noreply.github.com>
2020-03-29 18:50:06 -0400

    Update sort.h

--- hw/hw2/sort.h
+++ hw/hw2/sort.h
@@ -8,17 +8,4 @@
 #ifndef SORT_H
 #define SORT_H
 void sort(int numbers_count, int *nums);
-
-=======
-  
-/**
- * Constance Xu
- * I pledge my honor that I have abided by the Stevens Honor System.
- * header file for sort.c
-*/
-#ifndef SORT_H
-#define SORT_H
-void sort(int numbers_count, int *nums);
-
->>>>>>> 9203636e9dfbe2b966b9ab35ed6b65f5eab9e65a
 #endif
